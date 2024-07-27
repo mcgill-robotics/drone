2f8347b1de047ce74466656e318373ba31d2aad3
Imad Issafras <101837444+UnHappySquid@users.noreply.github.com>
2024-07-27 15:33:20 -0400

    fixed setup_ubuntu.sh

--- setup_scripts/setup_ubuntu.sh
+++ setup_scripts/setup_ubuntu.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -euxo pipefail
 sudo apt update && sudo apt upgrade -y
+# Install X11 server 
 sudo apt install -y xorg xinit
+
 # Install i3
 /usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2024.03.04_all.deb ~/keyring.deb SHA256:f9bb4340b5ce0ded29b7e014ee9ce788006e9bbfe31e96c09b2118ab91fca734
 sudo apt install ~/keyring.deb
@@ -9,8 +11,10 @@ echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-relea
 sudo apt update
 sudo apt install -y i3
 sudo apt install -y terminator
+
+# setup i3 and display on boot
 sudo printf "#!/bin/bash\nexec i3" > ~/.xinitrc
-sudo printf "if [[ -z \$DISPLAY ]] && [[ \$(tty) = /dev/tty1 ]]; then \n\tstartx\nfi" >> ~/.bashrc
+sudo printf "\nif [[ -z \$DISPLAY ]] && [[ \$(tty) = /dev/tty1 ]]; then \n\tstartx\nfi\n" >> ~/.bashrc
 sudo mkdir -p ~/.config/i3
 sudo mv config ~/.config/i3/
 sudo reboot
