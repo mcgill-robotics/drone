c566c7907a9fecd08dc38d509c1e2b996ce9bb98
Imad Issafras <101837444+UnHappySquid@users.noreply.github.com>
2024-07-28 14:02:08 -0400

    updated tool installation scrupt

--- setup_scripts/setup_tools.sh
+++ setup_scripts/setup_tools.sh
@@ -4,15 +4,11 @@ set -euxo pipefail
 cd
 git clone https://github.com/PX4/PX4-Autopilot.git --recursive
 
-# gazebo classic
+# ROS humble 
 sudo apt update && sudo apt install -y locales
 sudo locale-gen en_US en_US.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
-sudo apt install -y software-properties-common
-curl -sSL http://get.gazebosim.org | sh
-
-# ROS humble 
 sudo apt update && sudo apt install curl -y
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
@@ -22,4 +18,9 @@ sudo apt install -y ros-dev-tools
 source /opt/ros/humble/setup.bash && echo "source /opt/ros/humble/setup.bash" >> .bashrc
 
 # PX4 dependencies
-bash ./PX4-Autopilot/Tools/setup/ubuntu.sh --no-sim-tools
+bash ./PX4-Autopilot/Tools/setup/ubuntu.sh
+
+# gazebo classic
+sudo apt remove gz-garden
+sudo apt install aptitude
+sudo aptitude install gazebo libgazebo11 libgazebo-dev
