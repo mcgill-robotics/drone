7e83d9df91822da6a7565a8818e4fa902f2fc46b
UnHappySquid <imad.issafras@mail.mcgill.ca>
2024-11-05 14:22:43 -0500

    got first version of monitoring going

--- drone_ws/src/px4_controller/src/drone_node.cpp
+++ drone_ws/src/px4_controller/src/drone_node.cpp
@@ -12,36 +12,104 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <connection_result.h>
 #include <functional>
+#include <geometry_msgs/msg/detail/point_stamped__struct.hpp>
 #include <memory>
 
+#include "geometry_msgs/msg/point_stamped.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
-
 #include <mavsdk/mavsdk.h>
 #include <mavsdk/plugins/action/action.h>
 #include <mavsdk/plugins/telemetry/telemetry.h>
-using std::placeholders::_1;
+#include <rclcpp/logging.hpp>
+#include <rclcpp/publisher.hpp>
+#include <rclcpp/timer.hpp>
+#include <string>
 
-class MinimalSubscriber : public rclcpp::Node {
+using namespace std::chrono_literals;
+
+class OffboardNode : public rclcpp::Node {
 public:
-  MinimalSubscriber() : Node("minimal_subscriber") {
-    subscription_ = this->create_subscription<std_msgs::msg::String>(
-        "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+  OffboardNode() : Node("offboard_controller") {
+    // ROS stuff
+    monitoring_topic = this->create_publisher<geometry_msgs::msg::PointStamped>(
+        "drone_position", 10);
+
+    // MAVSDK stuff
+    monitoring_topic = this->create_publisher<geometry_msgs::msg::PointStamped>(
+        "drone_position", 10);
+    mavsdk = std::make_unique<mavsdk::Mavsdk>(mavsdk::Mavsdk::Configuration{
+        mavsdk::Mavsdk::ComponentType::GroundStation});
+    mavsdk::ConnectionResult con_res =
+        mavsdk->add_any_connection("udp://:14540");
+
+    if (con_res != mavsdk::ConnectionResult::Success) {
+      RCLCPP_ERROR(
+          this->get_logger(),
+          "Cannot connect to drone, maybe modify the connection address");
+      std::exit(1);
+    }
+
+    sys = mavsdk->first_autopilot(3.0);
+    if (!sys) {
+      RCLCPP_ERROR(this->get_logger(), "Timed out waiting for system");
+      std::exit(2);
+    }
+
+    mavsdk_telem = std::make_unique<mavsdk::Telemetry>(sys.value());
+    const auto set_rate_res = mavsdk_telem->set_rate_position(0.5);
+    if (set_rate_res != mavsdk::Telemetry::Result::Success) {
+      RCLCPP_ERROR(this->get_logger(), "Couldn't set rate for telemetry %d",
+                   set_rate_res);
+      std::exit(3);
+    }
+
+    mavsdk_telem->subscribe_position([this](mavsdk::Telemetry::Position pos) {
+      auto msg = geometry_msgs::msg::PointStamped();
+      msg.point.x = pos.latitude_deg;
+      msg.point.y = pos.longitude_deg;
+      msg.point.z = pos.relative_altitude_m;
+      this->monitoring_topic->publish(msg);
+    });
+
+    // subscription_ = this->create_subscription<std_msgs::msg::String>(
+    //     "topic", 10, std::bind(&OffboardNode::topic_callback, this,
+    //     std::placeholders::_1));
+    // timer_ = this->create_wall_timer(
+    //     500ms, std::bind(&OffboardNode::monitoring_callback, this));
   }
 
 private:
-  void topic_callback(const std_msgs::msg::String &msg) const {
-    RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg.data.c_str());
-  }
-  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+  // void monitoring_callback() {
+  //   auto msg = geometry_msgs::msg::PointStamped();
+  //   msg.point.x = 10.;
+  //   msg.point.y = 20.;
+  //   msg.point.z = 30.;
+  //   monitoring_topic->publish(msg);
+  // }
+
+  // void topic_callback(const std_msgs::msg::String &msg) const {
+  //   RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg.data.c_str());
+  // }
+
+  // rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+
+  // ROS2 related stuff
+  rclcpp::Publisher<geometry_msgs::msg::PointStamped>::SharedPtr
+      monitoring_topic;
+  rclcpp::TimerBase::SharedPtr timer_;
+
+  // mavsdk related stuff
+  std::optional<std::shared_ptr<mavsdk::System>> sys;
+  std::unique_ptr<mavsdk::Mavsdk> mavsdk;
+  std::unique_ptr<mavsdk::Telemetry> mavsdk_telem;
 };
 
 int main(int argc, char *argv[]) {
-  mavsdk::Mavsdk mavsdk{mavsdk::Mavsdk::Configuration{
-      mavsdk::Mavsdk::ComponentType::GroundStation}};
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalSubscriber>());
+  rclcpp::spin(std::make_shared<OffboardNode>());
   rclcpp::shutdown();
   return 0;
 }
