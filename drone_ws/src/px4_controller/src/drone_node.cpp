afbb3b7fe40400b02c807121633324ddaa393e4f
UnHappySquid <imad.issafras@mail.mcgill.ca>
2025-01-24 18:06:42 -0500

    Started actually refactoring, QC mode most likely done

--- drone_ws/src/px4_controller/src/drone_node.cpp
+++ drone_ws/src/px4_controller/src/drone_node.cpp
@@ -25,9 +25,23 @@
 
 using namespace std::chrono_literals;
 
+template <typename... Args>
+std::string string_format(const std::string &format, Args... args) {
+  int size_s = std::snprintf(nullptr, 0, format.c_str(), args...) +
+               1; // Extra space for '\0'
+  if (size_s <= 0) {
+    throw std::runtime_error("Error during formatting.");
+  }
+  auto size = static_cast<size_t>(size_s);
+  std::unique_ptr<char[]> buf(new char[size]);
+  std::snprintf(buf.get(), size, format.c_str(), args...);
+  return std::string(buf.get(),
+                     buf.get() + size - 1); // We don't want the '\0' inside
+}
+
 class OffboardNode : public rclcpp::Node {
 public:
-  OffboardNode() : Node("offboard_controller") {
+  OffboardNode() : Node("offboard_controller"), qc_tolerance(1.) {
 
     // tf2 stuff
     tf_world_broadcaster =
@@ -40,6 +54,8 @@ public:
     // MAVSDK stuff
     mavsdk = std::make_unique<mavsdk::Mavsdk>(mavsdk::Mavsdk::Configuration{
         mavsdk::Mavsdk::ComponentType::GroundStation});
+
+    // TODO: Modify the connection url when connected to the PixHawk
     mavsdk::ConnectionResult con_res =
         mavsdk->add_any_connection("udp://:14540");
 
@@ -59,8 +75,9 @@ public:
     mavsdk_telem = std::make_unique<mavsdk::Telemetry>(sys.value());
     mavsdk_action = std::make_unique<mavsdk::Action>(sys.value());
     mavsdk_offboard = std::make_unique<mavsdk::Offboard>(sys.value());
-    const mavsdk::Offboard::VelocityNedYaw stay{};
-    mavsdk_offboard->set_velocity_ned(stay);
+    // const mavsdk::Offboard::VelocityNedYaw stay{};
+    mavsdk::Offboard::PositionNedYaw stay_pos{};
+    mavsdk_offboard->set_position_ned(stay_pos);
 
     this->setup_monitoring();
 
@@ -74,8 +91,8 @@ public:
                   std::placeholders::_1));
 
     pop_sub = this->create_subscription<std_msgs::msg::Empty>(
-        "pop_command", 10,
-        std::bind(&OffboardNode::dequeue_topic_callback, this,
+        "cancel_command", 10,
+        std::bind(&OffboardNode::cancel_command_topic_callback, this,
                   std::placeholders::_1));
 
     this->timer_ = this->create_wall_timer(
@@ -88,33 +105,20 @@ public:
 private:
   // This only makes sense in the context of the action queue
   struct Command {
-    enum FlightMode {
-      QC = custom_msgs::msg::DroneState::MC,
-      FW = custom_msgs::msg::DroneState::FW
-    };
-
-    struct GoToTuple {
-      GoToTuple(double target_n, double target_e, double target_d,
-                double target_vn, double target_ve, double target_vd) {
-        n = target_n;
-        e = target_e;
-        d = target_d;
-        vn = target_vn;
-        ve = target_ve;
-        vd = target_vd;
-      }
-      double n, e, d, vn, ve, vd;
-    };
-    // Returns true if its done, false otherwise
+    // both perform tick and cancel_command return true if they are done, false
+    // otherwise
     virtual bool perform_tick(OffboardNode &on) = 0;
-    virtual bool is_goto() { return false; }
-    virtual std::optional<GoToTuple> get_goto_tuple() { return std::nullopt; }
-    virtual std::optional<FlightMode> get_desired_fm() { return std::nullopt; }
+    virtual bool cancel_command(OffboardNode &on) {
+      RCLCPP_INFO(on.get_logger(), "This command is not cancelable");
+      return perform_tick(on);
+    }
+    virtual std::string get_description() = 0;
+
     virtual ~Command() {}
   };
 
   struct TakeOff : Command {
-    TakeOff(double height) { this->target_height = height; }
+    TakeOff(double height) : target_height(height) {}
 
     virtual bool perform_tick(OffboardNode &node) override {
       if (node.state.in_air &&
@@ -132,6 +136,10 @@ private:
       }
 
       return false;
+    }
+
+    virtual std::string get_description() override {
+      return string_format("Taking off: target height %lf", target_height);
     }
 
     ~TakeOff() {}
@@ -154,140 +162,188 @@ private:
       return false;
     }
 
+    virtual std::string get_description() override {
+      return string_format("Landing");
+    }
+
     ~Land() {}
   };
 
-  struct GoTo : Command {
-    GoTo(double n, double e, double d, double vn, double ve, double vd,
-         FlightMode fm) {
+  struct GoToQC : Command {
+    GoToQC(double n, double e, double d)
+        : target_n(n), target_e(e), target_d(d) {}
 
-      target_n = n;
-      target_e = e;
-      target_d = d;
-
-      target_vn = vn;
-      target_ve = ve;
-      target_vd = vd;
-
-      desired_mode = fm;
-    }
-
-    virtual bool is_goto() override { return true; }
-
-    virtual std::optional<GoToTuple> get_goto_tuple() override {
-      return std::optional<GoToTuple>(GoToTuple(
-          target_n, target_e, target_d, target_vn, target_ve, target_vd));
-    };
-
-    virtual std::optional<FlightMode> get_desired_fm() override {
-      return std::optional<FlightMode>(desired_mode);
-    }
-
-    // TODO: Finish implementing velocity and yaw control
     virtual bool perform_tick(OffboardNode &node) override {
-      if (!start_pos_recorded) {
-        start_pos_recorded = true;
-        start_n = node.state.n;
-        start_e = node.state.e;
-        start_d = node.state.d;
-      }
-
-      // switch to offboard if not already
-      if (node.state.flight_mode != custom_msgs::msg::DroneState::OFFBOARD) {
-        node.mavsdk_offboard->start();
-      }
-
-      // send transition command if need to and if not sent already
-      if (node.state.vtol_state != desired_mode && !transtion_order_sent) {
-        mavsdk::Action::Result res;
-        if (desired_mode == FlightMode::QC) {
-          res = node.mavsdk_action->transition_to_multicopter();
-        } else {
-          res = node.mavsdk_action->transition_to_fixedwing();
-        }
-        if (res == mavsdk::Action::Result::Success)
-          transtion_order_sent = true;
-        return false;
-      }
-
       double dist_from_target = sqrt(pow((node.state.n - target_n), 2) +
                                      pow((node.state.e - target_e), 2) +
                                      pow((node.state.d - target_d), 2));
 
-      // if above tolerance and command to go somewhere wasnt sent already
-      if (((desired_mode == FlightMode::QC && dist_from_target >= 1.) ||
-           (desired_mode == FlightMode::FW && dist_from_target >= 10.)) &&
-          !goto_order_sent) {
+      if (dist_from_target >= node.qc_tolerance && !goto_command_sent) {
         mavsdk::Offboard::PositionNedYaw pos;
         pos.north_m = target_n;
         pos.east_m = target_e;
         pos.down_m = target_d;
-
         pos.yaw_deg = NAN;
-        // If there is a next goto action
-        if (node.command_deque.size() >= 1 &&
-            node.command_deque[0]->is_goto()) {
 
-          // if next goto action is in FW and we're in QC: setup yaw and
-          // actually over shoot the point so you dont slow down before trying to transition
-          if (desired_mode == FlightMode::QC &&
-              node.command_deque[0]->get_desired_fm().value() ==
-                  FlightMode::FW) {
-
-            double overshoot_scale = 1.1;
-            pos.north_m = start_n + (target_n - start_n) * overshoot_scale;
-            pos.east_m = start_e + (target_e - start_e) * overshoot_scale;
-
-            double next_n, next_e, d_n, d_e;
-            GoToTuple next_goto =
-                node.command_deque[0]->get_goto_tuple().value();
-            next_n = next_goto.n;
-            next_e = next_goto.e;
-
-            d_n = next_n - target_n;
-            d_e = next_e - target_e;
-
-            double angle = atan2(d_e, d_n) * 180. / M_PI;
-            // atan2 gives -180 to 180 while we want 0 to 360 for yaw control
-            if (angle < 0)
-              angle = 360. + angle;
-
-            pos.yaw_deg = angle;
-            RCLCPP_INFO(node.get_logger(),
-                        "next point is in FW, we're in QC, it is at (%f,%f) "
-                        "from current target, yaw needed is %f",
-                        d_n, d_e, pos.yaw_deg);
-          }
-        }
-
-        auto res = node.mavsdk_offboard->set_position_ned(pos);
-        if (res == mavsdk::Offboard::Result::Success)
-          goto_order_sent = true;
+        auto result = node.mavsdk_offboard->set_position_ned(pos);
+        if (result == mavsdk::Offboard::Result::Success)
+          goto_command_sent = true;
+      }
+      if (node.state.flight_mode != custom_msgs::msg::DroneState::OFFBOARD) {
+        node.mavsdk_offboard->start();
+        return false;
       }
 
-      // if within tolerance and in the desired mode, you're done !
-      if ((((desired_mode == FlightMode::QC && dist_from_target < 1.) ||
-            (desired_mode == FlightMode::FW && dist_from_target < 10.))) &&
-          node.state.vtol_state == desired_mode) {
-        // if (node.command_deque.size() == ) {
-        //   node.mavsdk_offboard->stop();
-        // }
-        return true;
-      }
-      return false;
+      return dist_from_target < node.qc_tolerance;
     }
 
-    ~GoTo() {}
+    virtual std::string get_description() override {
+      return string_format("Going to %lf, %lf, %lf", target_n, target_e,
+                           target_d);
+    }
 
-    double start_n, start_e, start_d;
-    double target_n, target_e, target_d, target_vn, target_ve, target_vd;
-    FlightMode desired_mode;
+    virtual bool cancel_command(OffboardNode &node) override { return true; }
 
-    // toggles to not overwhelm the flight controller
-    bool start_pos_recorded = false;
-    bool transtion_order_sent = false;
-    bool goto_order_sent = false;
+    ~GoToQC() {}
+    double target_n, target_e, target_d;
+    bool goto_command_sent = false;
   };
+
+  // TODO: Do this
+  struct GoToFW : Command {};
+  struct Hold : Command {};
+
+  // struct GoTo : Command {
+  //   GoTo(double n, double e, double d, double vn, double ve, double vd,
+  //        FlightMode fm) {
+  //
+  //     target_n = n;
+  //     target_e = e;
+  //     target_d = d;
+  //
+  //     target_vn = vn;
+  //     target_ve = ve;
+  //     target_vd = vd;
+  //
+  //     desired_mode = fm;
+  //   }
+  //
+  //   virtual bool is_goto() override { return true; }
+  //
+  //   virtual std::optional<GoToTuple> get_goto_tuple() override {
+  //     return std::optional<GoToTuple>(GoToTuple(
+  //         target_n, target_e, target_d, target_vn, target_ve, target_vd));
+  //   };
+  //
+  //   virtual std::optional<FlightMode> get_desired_fm() override {
+  //     return std::optional<FlightMode>(desired_mode);
+  //   }
+  //
+  //   // TODO: Finish implementing velocity and yaw control
+  //   virtual bool perform_tick(OffboardNode &node) override {
+  //     if (!start_pos_recorded) {
+  //       start_pos_recorded = true;
+  //       start_n = node.state.n;
+  //       start_e = node.state.e;
+  //       start_d = node.state.d;
+  //     }
+  //
+  //     // switch to offboard if not already
+  //     if (node.state.flight_mode != custom_msgs::msg::DroneState::OFFBOARD) {
+  //       node.mavsdk_offboard->start();
+  //     }
+  //
+  //     // send transition command if need to and if not sent already
+  //     if (node.state.vtol_state != desired_mode && !transtion_order_sent) {
+  //       mavsdk::Action::Result res;
+  //       if (desired_mode == FlightMode::QC) {
+  //         res = node.mavsdk_action->transition_to_multicopter();
+  //       } else {
+  //         res = node.mavsdk_action->transition_to_fixedwing();
+  //       }
+  //       if (res == mavsdk::Action::Result::Success)
+  //         transtion_order_sent = true;
+  //       return false;
+  //     }
+  //
+  //     double dist_from_target = sqrt(pow((node.state.n - target_n), 2) +
+  //                                    pow((node.state.e - target_e), 2) +
+  //                                    pow((node.state.d - target_d), 2));
+  //
+  //     // if above tolerance and command to go somewhere wasnt sent already
+  //     if (((desired_mode == FlightMode::QC && dist_from_target >= 1.) ||
+  //          (desired_mode == FlightMode::FW && dist_from_target >= 10.)) &&
+  //         !goto_order_sent) {
+  //       mavsdk::Offboard::PositionNedYaw pos;
+  //       pos.north_m = target_n;
+  //       pos.east_m = target_e;
+  //       pos.down_m = target_d;
+  //
+  //       pos.yaw_deg = NAN;
+  //       // If there is a next goto action
+  //       if (node.command_deque.size() >= 1 &&
+  //           node.command_deque[0]->is_goto()) {
+  //
+  //         // if next goto action is in FW and we're in QC: setup yaw and
+  //         // actually over shoot the point so you dont slow down before
+  //         trying to transition if (desired_mode == FlightMode::QC &&
+  //             node.command_deque[0]->get_desired_fm().value() ==
+  //                 FlightMode::FW) {
+  //
+  //           double overshoot_scale = 1.1;
+  //           pos.north_m = start_n + (target_n - start_n) * overshoot_scale;
+  //           pos.east_m = start_e + (target_e - start_e) * overshoot_scale;
+  //
+  //           double next_n, next_e, d_n, d_e;
+  //           GoToTuple next_goto =
+  //               node.command_deque[0]->get_goto_tuple().value();
+  //           next_n = next_goto.n;
+  //           next_e = next_goto.e;
+  //
+  //           d_n = next_n - target_n;
+  //           d_e = next_e - target_e;
+  //
+  //           double angle = atan2(d_e, d_n) * 180. / M_PI;
+  //           // atan2 gives -180 to 180 while we want 0 to 360 for yaw control
+  //           if (angle < 0)
+  //             angle = 360. + angle;
+  //
+  //           pos.yaw_deg = angle;
+  //           RCLCPP_INFO(node.get_logger(),
+  //                       "next point is in FW, we're in QC, it is at (%f,%f) "
+  //                       "from current target, yaw needed is %f",
+  //                       d_n, d_e, pos.yaw_deg);
+  //         }
+  //       }
+  //
+  //       auto res = node.mavsdk_offboard->set_position_ned(pos);
+  //       if (res == mavsdk::Offboard::Result::Success)
+  //         goto_order_sent = true;
+  //     }
+  //
+  //     // if within tolerance and in the desired mode, you're done !
+  //     if ((((desired_mode == FlightMode::QC && dist_from_target < 1.) ||
+  //           (desired_mode == FlightMode::FW && dist_from_target < 10.))) &&
+  //         node.state.vtol_state == desired_mode) {
+  //       // if (node.command_deque.size() == ) {
+  //       //   node.mavsdk_offboard->stop();
+  //       // }
+  //       return true;
+  //     }
+  //     return false;
+  //   }
+  //
+  //   ~GoTo() {}
+  //
+  //   double start_n, start_e, start_d;
+  //   double target_n, target_e, target_d, target_vn, target_ve, target_vd;
+  //   FlightMode desired_mode;
+  //
+  //   // toggles to not overwhelm the flight controller
+  //   bool start_pos_recorded = false;
+  //   bool transtion_order_sent = false;
+  //   bool goto_order_sent = false;
+  // };
 
   // TODO: DO REST OF COMMAND CLASSES
 
@@ -436,6 +492,11 @@ private:
     state.header.stamp = this->get_clock()->now();
     transform.header.stamp = this->get_clock()->now();
     state.command_queue_size = command_deque.size();
+    if (command_deque.size() == 0) {
+      state.current_command = "None";
+    } else {
+      state.current_command = command_deque[0]->get_description();
+    }
     this->tf_drone_broadcaster->sendTransform(transform);
     this->monitoring_pub->publish(state);
   }
@@ -445,18 +506,19 @@ private:
     std::unique_ptr<OffboardNode::Command> com;
     switch (action.action_type) {
     case custom_msgs::msg::Action::HOLD:
+      // TODO: As in this and
 
       break;
     case custom_msgs::msg::Action::GOTO_ACTION:
-      OffboardNode::GoTo::FlightMode fm;
       if (action.vtol_config == custom_msgs::msg::Action::IN_FW) {
-        fm = OffboardNode::GoTo::FlightMode::FW;
+        // TODO: this
+        RCLCPP_INFO(this->get_logger(), "NOT IMPLEMENTED YET");
+        return;
       }
       if (action.vtol_config == custom_msgs::msg::Action::IN_QC) {
-        fm = OffboardNode::GoTo::FlightMode::QC;
+        com = std::make_unique<OffboardNode::GoToQC>(action.n, action.e,
+                                                     action.d);
       }
-      com = std::make_unique<OffboardNode::GoTo>(
-          action.n, action.e, action.d, action.vn, action.ve, action.vd, fm);
       break;
     case custom_msgs::msg::Action::LAND:
       com = std::make_unique<OffboardNode::Land>();
@@ -468,34 +530,37 @@ private:
     command_deque.push_back(std::move(com));
   }
 
-  void dequeue_topic_callback(const std_msgs::msg::Empty &em) {
+  void cancel_command_topic_callback(const std_msgs::msg::Empty &em) {
     if (command_deque.size() > 0)
-      command_deque.pop_front();
+      want_to_cancel = true;
   }
 
   void command_queue_callback() {
     // RCLCPP_INFO(this->get_logger(), "Command queue has size %d",
     //             command_deque.size());
     if (command_deque.size() == 0) {
-      // This is incase we havent reset things because we poped a command before
-      // it was done or something like that
       if (state.flight_mode == custom_msgs::msg::DroneState::OFFBOARD) {
         mavsdk_offboard->stop();
       }
-      if (state.flight_mode != custom_msgs::msg::DroneState::HOLD) {
-        mavsdk_action->hold();
-      }
+      // This may make it impossible for the Remote controller to take over,
+      // above should suffice if (state.flight_mode !=
+      // custom_msgs::msg::DroneState::HOLD && state.flight_mode !=
+      // custom_msgs::msg::DroneState::MANUAL) {
+      //   mavsdk_action->hold();
+      // }
       return;
     }
-    auto head = std::move(command_deque.front());
-    command_deque.pop_front();
+    bool is_done = (want_to_cancel) ? command_deque[0]->cancel_command(*this)
+                                    : command_deque[0]->perform_tick(*this);
 
-    bool is_done = head->perform_tick(*this);
-
-    if (!is_done) {
-      command_deque.push_front(std::move(head));
+    if (is_done) {
+      want_to_cancel = false;
+      command_deque.pop_front();
     }
   }
+
+  // flight parameters
+  double qc_tolerance;
 
   // ROS2 related stuff
   rclcpp::Publisher<custom_msgs::msg::DroneState>::SharedPtr monitoring_pub;
@@ -520,6 +585,7 @@ private:
 
   // Command queue
   std::deque<std::unique_ptr<OffboardNode::Command>> command_deque = {};
+  bool want_to_cancel = false;
 };
 
 int main(int argc, char *argv[]) {
